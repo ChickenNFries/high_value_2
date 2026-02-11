0f9a8b1e42e087edd0fab4f70e1a2cbdbffbf75d
Hardeep <hkahlon98@gmail.com>
2026-02-11 15:47:16 +0000

    Created unit test for class

--- tests/test_most_often.py
+++ tests/test_most_often.py
@@ -1,1 +1,21 @@
+# File: tests/test_most_often.py
+
 from lib.most_often import MostOften
+
+def test_most_often_instantiates_list_parameter():
+    my_list = [1, 1, 2, 3, 4]
+    mo = MostOften(my_list)
+    assert mo.starting_list == my_list
+
+def test_most_often_can_add_new_item_to_list():
+    mo = MostOften([1, 1, 2, 3, 4])
+    mo.add_new(9)
+    assert mo.starting_list == [1, 1, 2, 3, 4, 9]
+
+def test_most_often_returns_most_often():
+    mo = MostOften([1, 1, 2, 3, 4, 9])
+    assert mo.get_most_often() == 1
+
+def test_most_often_only_one_parameter():
+    mo = MostOften([1, 2])
+    assert mo.get_most_often() == "no clear winner"
